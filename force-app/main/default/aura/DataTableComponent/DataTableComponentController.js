5ff3b267df62b3daacc6e6e2672fc985efbca35c
prasanthg24 <prasanth287471@gmail.com>
2022-08-30 18:35:45 +0530

    Date Table Component Added

--- force-app/main/default/aura/DataTableComponent/DataTableComponentController.js
+++ force-app/main/default/aura/DataTableComponent/DataTableComponentController.js
@@ -1,19 +1,40 @@
 ({
     doInit : function(component, event, helper) {
 
-        component.set('v.columns', [
-            {label: 'Beer Order ID', fieldName: 'Name', type: 'text'},
-            {label: 'Amount', fieldName: 'Order_Amount__c', type: 'text'},
-            {label: 'Billing City', fieldName: 'Billing_City__c', type: 'text'},
-            {label: 'Country', fieldName: 'Billing_Country__c', type: 'text'},
-            {label: 'Postal Code', fieldName: 'Shipping_Postal_Code__c', type: 'text'},
+        var actions = [{
+            label : 'Show Details',
+            name : 'show_details',
+            iconName : 'action:preview'
+        },
+         {
+                           label : 'Delete',
+                           name : 'delete',
+                           iconName : 'action:delete'
+         }];
+
+     component.set('v.columns', [
+            {label: 'Beer Order ID', fieldName: 'Name', type: 'text',editable:true},
+            {label: 'Amount', fieldName: 'Order_Amount__c', type: 'text' ,editable:true},
+            {label: 'Billing City', fieldName: 'Billing_City__c', type: 'text',editable:true},
+            {label: 'Country', fieldName: 'Billing_Country__c', type: 'text',editable:true},
+            {label: 'Postal Code', fieldName: 'Shipping_Postal_Code__c', type: 'text',editable:true},
            
            
 
             
         ]);
+         /*  component.set('v.columns', [
+            {label: 'Account Name', fieldName: 'Name', type: 'text', editable:true},
+            {label: 'Industry', fieldName: 'Industry', type: 'text'},
+            {label: 'Rating', fieldName: 'Rating', type: 'text'},
+            {label: 'Phone', fieldName: 'Phone', type: 'text', editable:true},
+            {type : "action", typeAttributes : { rowActions : actions }}
+        ]);*/
 
-        var action = component.get('c.getdatatable');
+
+      var action = component.get('c.getdatatable');
+        
+        //var action = component.get('c.fetchAccount');
         action.setCallback(this, function(response){
             var state = response.getState();
             //alert(state);
@@ -29,4 +50,36 @@
         var selectedRows = event.getParam('selectedRows');
         console.log('selectedRows ', selectedRows);
     },
+    handleRowAction : function(component, event, helper) {
+        var action = event.getParam('action');
+        var row = event.getParam('row');
+        console.log( 'action ', action.name);
+        console.log(' row ', row.Id)
+        switch(action.name){
+            case 'show_details' :
+                alert(row.Id+' '+row.Name);
+                break;
+            case 'delete' :
+                var data = component.get('v.data');
+                var index = data.indexOf(row);
+                data.splice(index, 1);
+                // splice - 3 Params 
+                // 1- index (add/remove)
+                // no of items to add/remove
+                // optional list of items that we wanted to add
+                component.set('v.data', data);
+                break;
+        }
+    },
+    handleDraftValues : function(component, event, helper) {
+        var draftRecords = event.getParam('draftValues');
+        console.log(' draftRecords ', draftRecords);
+        var data = component.get('v.data');
+        for(let i=0; i<draftRecords.length; i++){
+            var id = draftRecords[i].id;
+            alert(draftRecords[i].id + '  Id = '+id.substring(4, id.length));
+            console.log(data[id.substring(4, id.length)]);
+        }
+    }
+
 })
