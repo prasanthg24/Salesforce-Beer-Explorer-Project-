db84d429f38ad35acb66f595b34c484e5309c8b0
prasanthg24 <prasanth287471@gmail.com>
2022-08-30 13:00:31 +0530

    Date Table Component Added

--- force-app/main/default/aura/DataTableComponent/DataTableComponentController.js
+++ force-app/main/default/aura/DataTableComponent/DataTableComponentController.js
@@ -1,5 +1,25 @@
 ({
-    myAction : function(component, event, helper) {
+    doInit : function(component, event, helper) {
 
-    }
+        component.set('v.columns', [
+            {label: 'Beer Order ID', fieldName: 'opportunityName', type: 'text'},
+           
+        ]);
+
+        var action = component.get('c.getdatatable');
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            //alert(state);
+            if(state === 'SUCCESS' || state==='DRAFT' ){
+                var responseValue = response.getReturnValue();
+                console.log('responseValue ', responseValue)
+                component.set('v.data', responseValue);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    doSelectRecord : function(component, event, helper) {
+        var selectedRows = event.getParam('selectedRows');
+        console.log('selectedRows ', selectedRows);
+    },
 })
