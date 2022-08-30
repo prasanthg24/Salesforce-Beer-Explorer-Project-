9df9d25bd7c79079ff8abbd13461b72fb8eab20c
prasanthg24 <prasanth287471@gmail.com>
2022-08-30 14:47:28 +0530

    Date Table Component Added

--- force-app/main/default/aura/DataTableComponent/DataTableComponentController.js
+++ force-app/main/default/aura/DataTableComponent/DataTableComponentController.js
@@ -2,7 +2,12 @@
     doInit : function(component, event, helper) {
 
         component.set('v.columns', [
-            {label: 'Beer Order ID', fieldName: 'opportunityName', type: 'text'},
+            {label: 'Beer Order ID', fieldName: 'Name', type: 'text'},
+            {label: 'Amount', fieldName: 'Order_Amount__c', type: 'text'},
+            {label: 'Beer Name', fieldName: 'Beer__c', type: 'text'},
+            {label: 'Postal_Code__c', fieldName: 'Shipping_Postal_Code__c', type: 'text'},
+            {label: 'Beer Order Record ID', fieldName: 'Id', type: 'text'}
+
            
         ]);
 
