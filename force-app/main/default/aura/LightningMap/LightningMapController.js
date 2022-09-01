0ba71ccf4775f895621f1b09f3a437a20f91dff5
prasanthg24 <prasanth287471@gmail.com>
2022-09-01 13:01:09 +0530

    lightning map code updated

--- force-app/main/default/aura/LightningMap/LightningMapController.js
+++ force-app/main/default/aura/LightningMap/LightningMapController.js
@@ -1,20 +1,65 @@
 ({
     init : function(component, event, helper)
     {
-        component.set('v.mapMarkers', [
+        component.set
+        ('v.mapMarkers', 
+        [
             {
-                location: {
-                    Street: 'Badrakaliamman Real-estate',
-                    City: 'Podanur',
-                    PostalCode: '641023',
+                location:
+                 {
+                    City: 'Vadavalli',
                     State: 'TamilNadu',
-                    Country: 'India'
+                    Country: 'India',
+                    Street:'NO 4 Marudham Nagar Extn',
+                    PostalCode: '641041'
+                }
+            },
+               { location:
+                {
+                    City: 'Podanur',
+                    State: 'TamilNadu',
+                    Country: 'India',
+                    Street:'13 Amman Pudur',
+                    PostalCode: '641023'
+                },
+                title :" Podanur "
+
+            },
+                {
+                    location: {
+                        City: 'Juan-les-Pins',
+                        Country: 'France'
+                    },
+    
+                    value: 'France6',
+                    icon: 'custom:custom74',
+                    title: 'Juan-les-Pins'
+                },
+                {
+                    location: {
+                        City: 'Cannes',
+                        Country: 'France'
+                    },
+    
+                    value: 'France7',
+                    icon: 'custom:custom3',
+                    title: 'Cannes'
+                },
+                {
+                    location: {
+                        City: 'Saint-Raphaël',
+                        Country: 'France'
+                    },
+    
+                    value: 'France8',
+                    icon: 'custom:custom54',
+                    title: 'Saint-Raphaël'
                 },
 
-                title: 'The White House',
-                description: 'Landmark, historic home & office of the United States president, with tours for visitors.'
-            }
-        ]);
+            
+        ] );
+       
+        
         component.set('v.zoomLevel', 10);
     }
 })
