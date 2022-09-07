cc4a21264e255d84b995df68a9c84826d9a07bef
prasanthg24 <prasanth287471@gmail.com>
2022-09-07 20:59:50 +0530

    code Added

--- force-app/main/default/aura/CartDetail/CartDetailHelper.js
+++ force-app/main/default/aura/CartDetail/CartDetailHelper.js
@@ -1,5 +1,10 @@
 ({
-    helperMethod : function() {
-
-    }
+    validate : function(component, event, helper) {
+        var isValid = component.find('address').reduce(function (validSoFar, inputCmp) {
+            inputCmp.showHelpMessageIfInvalid();
+            inputCmp.set('v.validity', {valid:false, badInput :true});
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        return isValid; 
+	}
 })
